923a9cbf469b4fb98b0576b1e13d4e6cee8c5b45
Tsukasa Murata <stepfootprint@gmail.com>
2021-05-06 05:56:58 +0900

    fakerのunique上限回避

--- database/factories/SellerFactory.php
+++ database/factories/SellerFactory.php
@@ -24,7 +24,7 @@ class SellerFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique(false, 10000000)->safeEmail,
+            'email' => Str::random(20) . '@' . Str::random(20),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
