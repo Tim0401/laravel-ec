ac04b6e242d833421d3d6b0d6d51375efba86ba8
Tsukasa Murata <stepfootprint@gmail.com>
2021-05-06 17:11:08 +0900

    navbar bugfix

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,15 +13,15 @@
                 <!-- Navigation Links -->
                 @if (Auth::check())
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        @if(Auth::guard('web')->check())
-                            <x-jet-nav-link href="{{ route($prefix . 'dashboard') }}" :active="request()->routeIs('dashboard')">
+                        @if(Auth::guard('web')->check() && empty($prefix))
+                            <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                                 {{ __('Dashboard') }}
                             </x-jet-nav-link>
                             <x-jet-nav-link href="{{ route('cart.index') }}" :active="request()->routeIs('cart.index')">
                                 {{ __('Cart') }}
                             </x-jet-nav-link>
                         @endif
-                        @if(Auth::guard('cms')->check())
+                        @if(Auth::guard('cms')->check() && !empty($prefix))
                             <x-jet-nav-link href="{{ route($prefix . 'dashboard') }}" :active="request()->routeIs('cms.dashboard')">
                                 {{ __('Dashboard') }}
                             </x-jet-nav-link>
