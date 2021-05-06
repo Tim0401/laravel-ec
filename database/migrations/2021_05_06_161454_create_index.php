79d3d20778e5448cf1cec0bb53ece13b8d85c68b
Tsukasa Murata <stepfootprint@gmail.com>
2021-05-06 16:30:45 +0900

    index追加

---
+++ database/migrations/2021_05_06_161454_create_index.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->index(['deleted_at']);
+            $table->index(['deleted_at', 'price']);
+            $table->index(['deleted_at', 'stock']);
+            $table->index(['deleted_at', 'created_at']);
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->index(['created_at']);
+        });
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->index(['created_at']);
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->index(['deleted_at']);
+        });
+        Schema::table('sellers', function (Blueprint $table) {
+            $table->index(['deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropIndex(['deleted_at']);
+            $table->dropIndex(['deleted_at', 'price']);
+            $table->dropIndex(['deleted_at', 'stock']);
+            $table->dropIndex(['deleted_at', 'created_at']);
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropIndex(['created_at']);
+        });
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->dropIndex(['created_at']);
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex(['deleted_at']);
+        });
+        Schema::table('sellers', function (Blueprint $table) {
+            $table->dropIndex(['deleted_at']);
+        });
+    }
+}
